b0a04101a367f0878a296a26ba681ab2f2479f40
Juho Pekkala <juho.pekkala@arcusys.fi>
2015-11-24 23:26:45 +0200

    Lisäyksiä

--- module_liiga.py
+++ module_liiga.py
@@ -4,29 +4,34 @@ from datetime import datetime
 
 def giev_liiga():
 
- announcement = "Liigageimit: "
- g = open('calendar.ics', 'rb')
- gcal = Calendar.from_ical(g.read())
- gamesToday = []
- for event in gcal.walk():
-  if event.name == "VEVENT":
-   game = event.get('summary')
-   gameDate = event.get('dtstart').dt.date()
-   dateToday = datetime.now().date()
+    announcement = "Liigageimit: "
+    g = open('calendar.ics', 'rb')
+    gcal = Calendar.from_ical(g.read())
+    gamesToday = []
+    
+    for event in gcal.walk():
+        if event.name == "VEVENT":
+        game = event.get('summary')
+        gameDate = event.get('dtstart').dt.date()
+        dateToday = datetime.now().date()
  
-   if gameDate == dateToday:
-    gamesToday.append(game)
+    if gameDate == dateToday:
+        gamesToday.append(game)
 
- g.close()
+    g.close()
 
- if not gamesToday:
-  return "No hockey for you!"
- else:
-  separateGames = ', '.join(gamesToday)
-  gamesToPrint = ''.join(separateGames)
-  games = announcement + gamesToPrint
-  return games
+    if not gamesToday:
+        print "No hockey for you!"
+        return "No hockey for you!"
+    else:
+        separateGames = ', '.join(gamesToday)
+        gamesToPrint = ''.join(separateGames)
+        games = announcement + gamesToPrint
+        print games
+        return games
 
 def command_liiga(bot, user, channel, args):
- return bot.say(channel, giev_liiga())
+    if not args:
+        return "Error!"
+    return bot.say(channel, giev_liiga())
 
