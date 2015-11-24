3c09174e7057c1f5b42bf8f2b0a94cd279eacb9c
Juho Pekkala <juho.pekkala@arcusys.fi>
2015-11-24 23:39:08 +0200

    siivousta

--- module_liiga.py
+++ module_liiga.py
@@ -21,17 +21,13 @@ def giev_liiga():
     g.close()
 
     if not gamesToday:
-        print "No hockey for you!"
         return "No hockey for you!"
     else:
         separateGames = ', '.join(gamesToday)
         gamesToPrint = ''.join(separateGames)
         games = announcement + gamesToPrint
-        print games
         return games
 
 def command_liiga(bot, user, channel, args):
-    if not args:
-        return "Error!"
     return bot.say(channel, giev_liiga())
 
